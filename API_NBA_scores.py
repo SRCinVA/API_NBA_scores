b825c3dc6e5a238de335eb38e958f83ab88d362d
SRCinVA <scitarella@hotmail.com>
2022-05-06 20:25:53 -0400

    started on some methods

--- API_NBA_scores.py
+++ API_NBA_scores.py
@@ -4,4 +4,15 @@ from pprint import PrettyPrinter
 BASE_URL = "https://data.nba.net"
 ALL_JSON = "/prod/v1/today.json"
 
-response = get(BASE_URL + ALL_JSON).json()
+# need this to make the output readable:
+printer = PrettyPrinter()
+
+def get_links():
+    data = get(BASE_URL + ALL_JSON).json()
+    links = data['links']
+    return links
+
+# printer.pprint(scoreboard)
+
+def get_scoreboard():
+    scoreboard = get_links()['currentScoreboard'] # builds on the previous method
